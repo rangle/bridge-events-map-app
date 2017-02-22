316374dbb0e76ddd60d1cd88273b2c37bccb21c5
Camilla Tabis <Camilla Tabis>
2017-02-21 19:53:36 -0500

    remove error due to inital state

--- src/reducers/eventDetailsReducer.js
+++ src/reducers/eventDetailsReducer.js
@@ -1,6 +1,6 @@
 import * as types from '../actions/actionTypes';
 
-export default function(state = [], action) {
+export default function(state = {}, action) {
   switch (action.type) {
 
   case types.ACTION_TYPES.LOAD_EVENT_DETAILS:
