b2c3ebc11a69c97ca0682962c86b98ed3d279699
basnataliia <basnataliia@gmail.com>
2017-02-23 20:17:33 -0500

    materil-ui theme is included in this project

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import routes from './store/routes';
 import configureStore from './store/configure-store';
@@ -15,14 +17,17 @@ import './styles/index.css';
 const store = configureStore({});
 const history = syncHistoryWithStore(browserHistory, store);
 
+injectTapEventPlugin();
 
 if (!__TEST__) {
   ReactDOM.render(
     <div>
       <Provider store={ store }>
-        <Router history={ history }>
-          { routes }
-        </Router>
+        <MuiThemeProvider>
+          <Router history={ history }>
+            { routes }
+          </Router>
+      </MuiThemeProvider>
       </Provider>
     </div>,
     document.getElementById('root')
